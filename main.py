d1970e90e57063f723c40377a456d05e421587ae
Vince Knight <vincent.knight@gmail.com>
2019-12-17 11:19:34 +0000

    Update the code to record the ranks of all strategies.

--- main.py
+++ main.py
@@ -17,54 +17,47 @@ def get_players():
     return first_tournament_participants_ordered_by_reported_rank
 
 
-def obtain_ranked_names(players, seed=0):
+def obtain_ranks(players, seed=0):
     """
     Run the tournament with the same parameters as reported in the original work
-    (5 repetitions and 200 turns) and return the ranked names.
+    (5 repetitions and 200 turns) and return the ranks.
     """
     axl.seed(seed)
-    tournament = axl.Tournament(
-        players=players, turns=200, repetitions=5
-    )
-    results = tournament.play(progress_bar=False)
-    return results.ranked_names
+    tournament = axl.Tournament(players=players, turns=200, repetitions=5)
+    ranking = tournament.play(progress_bar=False).ranking
+    return [ranking.index(i) for i, _ in enumerate(ranking)]
 
 
-def count_matches(ranked_names, ranked_players):
+def count_matches(ranks):
     """
     Count the number of ranks that match between the reported and reproduced
     tournaments.
     """
-    first_tournament_ranked_names = [str(p) for p in ranked_players]
-    return sum(
-        reported == reproduced
-        for reported, reproduced in zip(first_tournament_ranked_names, ranked_names)
-    )
+    return sum(reported == reproduced for reported, reproduced in enumerate(ranks))
 
 
-def write_data(seed, number, winner, tit_for_tat_rank, filename, mode="a"):
+def write_data(seed, number, ranks, filename, mode="a"):
     """
     Write the data to a csv file.
     """
     with open(filename, mode) as f:
         writer = csv.writer(f)
-        writer.writerow([seed, number, tit_for_tat_rank, winner])
+        writer.writerow([seed, number] + ranks)
 
 
 def check_seed(seed, players, filename):
     """
     Check a given seed and return how many ranks match.
     """
-    ranked_names = obtain_ranked_names(players=players, seed=seed)
-    number_of_matches = count_matches(ranked_names=ranked_names, ranked_players=players)
+    ranks = obtain_ranks(players=players, seed=seed)
 
-    write_data(
-        seed=seed,
-        number=number_of_matches,
-        tit_for_tat_rank=ranked_names.index("Tit For Tat"),
-        winner=ranked_names[0],
-        filename=filename,
-    )
+    # Check reproducibility
+    assert ranks == obtain_ranks(
+        players=players, seed=seed
+    ), f"Failed to reproduce for seed={seed}"
+
+    number_of_matches = count_matches(ranks=ranks)
+    write_data(seed=seed, number=number_of_matches, ranks=ranks, filename=filename)
 
     return number_of_matches
 
@@ -80,32 +73,41 @@ def main():
     try:
         seed = pd.read_csv("main.csv")["seed"].max() + 1
     except FileNotFoundError:
+        player_names = [p.name for p in players]
         seed = 0
         write_data(
             seed="seed",
             number="number",
-            winner="winner",
-            tit_for_tat_rank="tit_for_tat_rank",
+            ranks=player_names,
             filename=filename,
             mode="w",
         )
 
-    while check_seed(seed=seed, players=players, filename=filename) != number_of_players:
+    while (
+        check_seed(seed=seed, players=players, filename=filename) != number_of_players
+    ):
         seed += 1
+
 
 def summarise():
     """
     Analyse and display the current data collection
     """
+    players = get_players()
+    player_names = [p.name for p in players]
 
     df = pd.read_csv("main.csv")
-    print(df.describe())
-    print("All winners:")
-    print(collections.Counter(df["winner"]))
+
+    print(df[["seed", "number"]].describe())
+
+    print("Number of wins:")
+    print((df[player_names] == 0).sum(axis=0))
+
 
 if __name__ == "__main__":
     import sys
-    if "--summarise" in sys.argv:
+
+    if "-s" in sys.argv:
         summarise()
     else:
         main()
